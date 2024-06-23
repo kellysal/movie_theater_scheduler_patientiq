09d5d8fefc20d476705c793df19ec202d4245c6e
Kelly Salamack <kellysalamackdesign@gmail.com>
2024-06-23 19:27:21 -0400

    add movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+
+    attr_accessor :title, :year, :rating, :duration
+
+	def initialize(title, year, rating, duration=0)
+		@title = title
+		@year = year
+		@rating = rating
+		@duration = duration.to_i
+	end
+    
 end
