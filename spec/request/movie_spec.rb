492047864c596396fb8a2edc2fff8b3812a0ab27
Kelly Salamack <kellysalamackdesign@gmail.com>
2024-06-23 21:15:58 -0400

    add spec + input

---
+++ spec/request/movie_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+# require 'pry'
+
+RSpec.describe "Movies", type: :request do
+
+  data = JSON.parse(File.read("spec/input.json"))
+
+  describe "POST /movies" do
+    it "returns http success" do
+      
+  		headers = { "ACCEPT" => "application/json" }
+    	post "/movies",
+      :params => data,
+      :headers => headers,
+      as: :json
+
+      expect(response).to have_http_status(:success)
+      # binding.pry
+    end
+
+  end
+
+end
